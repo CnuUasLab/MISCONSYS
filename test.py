c495e41d008a3e46500082d05ab4dee69697a6b2
vixadd <dkroell@acm.org>
2017-11-26 01:23:15 -0500

    Added testing scripts along with integration
    Integration is through the travis.yml script that is parsed. Testing is for the Singleton

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
+#=============================================#
+#                                             #
+#	Test cases for the python classes     #
+#              Author: David Kroell           #
+#             Version: 0.0.1                  #
+#                                             #
+#=============================================#
+
 import sys
 
+# Import interoperability classes for
+# unit tests.
 sys.path.insert(0, "./Interoperability/src/")
 import singly
 
-def Singleton_Test():
-	return singly.test
+import pytest
 
-def test_Interoperability():
-	assert Singleton_Test()
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
 
-
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
