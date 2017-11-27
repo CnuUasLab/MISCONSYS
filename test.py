7db5b5c7a803f88bb5a25070ec140cc68f7f9d75
vixadd <dkroell@acm.org>
2017-11-27 00:37:58 -0500

    Updated Singleton class naming conventions with CI
    Singleton class name change for decorator,
    Continuous Integration support for future builds

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-#=============================================#
-#                                             #
-#	Test cases for the python classes     #
-#              Author: David Kroell           #
-#             Version: 0.0.1                  #
-#                                             #
-#=============================================#
+#=====================================#
+#                                     #
+#	Testing for unittests in all  #
+#          Python modules             #
+#                                     #
+#           Author: David Kroell      #
+#          Version: 0.0.1             #
+#                                     #
+#=====================================#
 
 import sys
 
-# Import interoperability classes for
-# unit tests.
+
 sys.path.insert(0, "./Interoperability/src/")
-import singly
+import singleton
 
-import pytest
 
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-
-def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
-        capital_case(9)
+def test_singleton():
+     assert singleton.test()
