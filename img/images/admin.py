abe2b21d30177b351866c96a7d7201287190a34d
vixadd <dkroell@acm.org>
2018-04-06 09:48:29 -0400

    dump

--- img/images/admin.py
+++ img/images/admin.py
@@ -1,6 +1,18 @@
+#==================================================#
+#       Admin control for the django app           #
+#    Allows for admin controls in the context of   #
+#    the images being modified.                    #
+#                                                  #
+#               Author: David Kroell               #
+#              Version: 0.0.1                      #
+#                                                  #
+#==================================================#
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Image, Target
 
-# Register your models here.
+admin.site.register(Image)
+admin.site.register(Target)
